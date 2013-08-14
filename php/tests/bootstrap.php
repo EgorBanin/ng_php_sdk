e9868aa2f866d2e881de46feb4df37686393c88d
Egor Banin <egor.banin@gmail.com>
2013-08-14 13:52:03 +0400

    костыль для бага с повторной загрузкой bootstrap.php

--- php/tests/bootstrap.php
+++ php/tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-const SRC_PATH = '../src/';
-$srcPath = realpath(__DIR__.DIRECTORY_SEPARATOR.SRC_PATH);
+$SRC_PATH = '../src/';
+$srcPath = realpath(__DIR__.DIRECTORY_SEPARATOR.$SRC_PATH);
 set_include_path(get_include_path().PATH_SEPARATOR.$srcPath);
